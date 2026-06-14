070707d8e8c3205e8e831710fcd9633df5e87dbe
Piyush verma <piyush.verma2023@ssipmt.com>
2026-06-14 12:08:00 +0530

    Time: 0 ms (100%), Space: 120.6 MB (96.43%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -1,18 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
+
+    ListNode* middleNode(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast->next && fast->next->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+
+    ListNode* reverse(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        while(curr){
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+
     int pairSum(ListNode* head) {
+        ListNode* middle = middleNode(head);
+        ListNode* second = middle->next;
+        middle->next = NULL;
+        ListNode* first = reverse(head);
         int res = 0;
-        ListNode* left = head;
-
-        auto dfs = [&](this auto&& dfs, ListNode* right) -> void {
-            if (right->next)
-                dfs(right->next);
-
-            res = max(res, left->val + right->val);
-            left = left->next;
-        };
-
-        dfs(head);
+        while(first){
+            res = max(res,first->val + second->val);
+            first = first->next;
+            second = second->next;
+        }
         return res;
     }
 };
